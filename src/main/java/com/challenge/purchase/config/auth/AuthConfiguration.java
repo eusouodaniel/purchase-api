c33d192c6249e39ed4d1dc7d912e2fa7c3864f27
Daniel Rodrigues da Silva <daniel.rodrigues@192.168.1.10>
2021-03-28 15:54:30 -0300

    NEW: Implement spring security

---
+++ src/main/java/com/challenge/purchase/config/auth/AuthConfiguration.java
@@ -0,0 +1,35 @@
+package com.challenge.purchase.config.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class AuthConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/products").permitAll()
+		.antMatchers(HttpMethod.GET, "/products/*").permitAll()
+		.antMatchers(HttpMethod.GET, "/categories").permitAll()
+		.antMatchers(HttpMethod.GET, "/categories/*").permitAll()
+		.anyRequest().authenticated();
+	}
+	
+	@Override
+	public void configure (WebSecurity web) throws Exception {
+		
+	}
+}
+
--- src/main/java/com/challenge/purchase/model/User.java
+++ src/main/java/com/challenge/purchase/model/User.java
@@ -1,6 +1,7 @@
 package com.challenge.purchase.model;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,8 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class User {
+public class User implements UserDetails {
 	
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -63,6 +67,31 @@ public class User {
 	}
 	public void setDateCreation(LocalDateTime dateCreation) {
 		this.dateCreation = dateCreation;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
