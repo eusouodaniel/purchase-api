377e9aa278b7aedec07e0df33715882358eb229d
Daniel Rodrigues da Silva <daniel.rodrigues@192.168.1.10>
2021-03-28 15:06:38 -0300

    NEW: Logger in service category

--- src/main/java/com/challenge/purchase/service/CategoryService.java
+++ src/main/java/com/challenge/purchase/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.challenge.purchase.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,13 +13,15 @@ import com.challenge.purchase.model.Category;
 import com.challenge.purchase.repository.CategoryRepository;
 
 @Service
-public class CategoryService {
-	
+public class CategoryService implements BaseService {
+
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
 	public List<CategoryDto> listAll() {
 		List<Category> categories = categoryRepository.findAll();
+		
+		logger.info("ListAll categories");
 		
 		return CategoryDto.convert(categories);
 	}
@@ -27,20 +30,27 @@ public class CategoryService {
 		Category category = form.convert();
 		categoryRepository.save(category);
 		
+		logger.info("New category inserted - ID: {} - Date: {}", category.getId(), LocalDateTime.now());
+		
 		return category;
 	}
 	
 	public Optional<Category> getCategory(Long id) {
+		logger.info("Get category");
+		
 		return categoryRepository.findById(id);
 	}
 	
 	public Category update(Long id, CategoryForm form) {
 		Category category = form.update(id, categoryRepository);
 		
+		logger.info("Category has edited - ID: {} - Date: {}", category.getId(), LocalDateTime.now());
+		
 		return category;
 	}
 	
 	public void delete(Long id) {
 		categoryRepository.deleteById(id);
+		logger.info("Category has deleted - ID: {} - Date: {}", id, LocalDateTime.now());
 	}
 }
